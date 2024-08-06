b312c1bda8a004922e9a73731f3d8a80bf372b3a
Muaaz Anwar <maazweber@gmail.com>
2024-08-06 12:15:29 -0700

    used the reducer hook

--- first_react_app/src/components/todo/Todo.jsx
+++ first_react_app/src/components/todo/Todo.jsx
@@ -1,34 +1,58 @@
-import { useState, useRef } from "react";
+import { useReducer, useRef } from "react";
 import Todo_item from "./Todo_item";
-import {items} from './Store/Context';
+import { items } from './Store/Context';
 import { IoIosAddCircle } from "react-icons/io";
+const reducerfun = (CurrentState, action) => {
+    let newval = CurrentState;
+    if (action.type == "NEW_ITEM") {
+            if (action.payload.name != "" && action.payload.date != "") {
+                newval = [...CurrentState, {
+                    name: action.payload.name,
+                    date: action.payload.date
+                }];
+            }
+            }else if (action.type == "DEL_ITEM") {
+                let name = action.payload.name;
+                newval = CurrentState.filter(tasking => tasking.name != name);
+            }
+              return newval;
+}
 function Todo() {
-    let [tasking, settasking] = useState([]);
+    // let [tasking, settasking] = useState([]);
+    let [tasking, dispatchtodoitem] = useReducer(reducerfun, []);
+
     let refname = useRef();
     let reftask = useRef();
     let clicked = (event) => {
+    event.preventDefault();
+    let date = reftask.current.value;
+    const name = refname.current.value;
 
-        let tasked = reftask.current.value;
-        const named = refname.current.value;
-        event.preventDefault();
-        if (named != "" && tasked != "") {
-            let newarray = [...tasking, {
-                name: named,
-                date: tasked
-            }];
-            settasking(newarray);
-            reftask.current.value = "";
-            refname.current.value = "";
+        const newfunitem = {
+            type: "NEW_ITEM",
+            payload: {
+                name,
+                date
+            }
         }
+        dispatchtodoitem(newfunitem);
+        reftask.current.value = "";
+        refname.current.value = "";
     }
     let deleteitem = (name) => {
-        const finalitem = tasking.filter(tasking => tasking.name != name);
-        settasking(finalitem)
+        
+        const newfunitem = {
+            type: "DEL_ITEM",
+            payload: {name}
+        }
+        dispatchtodoitem(newfunitem);
+        // settasking(finalitem)
     }
     return <>
         <items.Provider value={{
             tasking: tasking,
-            deleteitem: deleteitem,}}>
+            deleteitem: deleteitem,
+        }}>
             <div className="container mt-3">
                 <h1 style={{ textAlign: 'center', fontWeight: '800', color: 'white', backgroundColor: 'black', padding: '10px 0px' }} className="mb-3">Task 1</h1>
                 <form className="row" onSubmit={clicked}>

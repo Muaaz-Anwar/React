78a367c8143abcb3d8d13cfe87f0234390d66ea5
Muaaz Anwar <maazweber@gmail.com>
2024-08-20 09:07:37 -0700

    this

--- first_react_app/src/components/todo/Store/Context.jsx
+++ first_react_app/src/components/todo/Store/Context.jsx
@@ -1,6 +1,67 @@
 import { createContext } from "react";
+import { useReducer, useRef } from "react";
 
 export const items = createContext({
     tasking: [],
-    deleteitem: () => {}
+    deleteitem: () => { }
 });
+
+
+
+const reducerfun = (CurrentState, action) => {
+    let newval = CurrentState;
+    if (action.type == "NEW_ITEM") {
+        if (action.payload.name != "" && action.payload.date != "") {
+            newval = [...CurrentState, {
+                name: action.payload.name,
+                date: action.payload.date
+            }];
+        }
+    } else if (action.type == "DEL_ITEM") {
+        let name = action.payload.name;
+        newval = CurrentState.filter(tasking => tasking.name != name);
+    }
+    return newval;
+}
+
+
+const TodoItemServiceProvider = ({ children }) => {
+    // let [tasking, settasking] = useState([]);
+    let [tasking, dispatchtodoitem] = useReducer(reducerfun, []);
+
+    let refname = useRef();
+    let reftask = useRef();
+    let clicked = (event) => {
+        event.preventDefault();
+        let date = reftask.current.value;
+        const name = refname.current.value;
+
+        const newfunitem = {
+            type: "NEW_ITEM",
+            payload: {
+                name,
+                date
+            }
+        }
+        dispatchtodoitem(newfunitem);
+        reftask.current.value = "";
+        refname.current.value = "";
+    }
+    let deleteitem = (name) => {
+
+        const newfunitem = {
+            type: "DEL_ITEM",
+            payload: { name }
+        }
+        dispatchtodoitem(newfunitem);
+        // settasking(finalitem)
+    }
+    return <items.Provider value={{
+        tasking: tasking,
+        deleteitem: deleteitem,
+    }}>
+    {children}
+    </items.Provider>
+
+}
+export default TodoItemServiceProvider
--- first_react_app/src/components/todo/Todo.jsx
+++ first_react_app/src/components/todo/Todo.jsx
@@ -1,66 +1,20 @@
-import { useReducer, useRef } from "react";
+
 import Todo_item from "./Todo_item";
-import { items } from './Store/Context';
+import TodoItemServiceProvider from './Store/Context'
 import { IoIosAddCircle } from "react-icons/io";
-const reducerfun = (CurrentState, action) => {
-    let newval = CurrentState;
-    if (action.type == "NEW_ITEM") {
-            if (action.payload.name != "" && action.payload.date != "") {
-                newval = [...CurrentState, {
-                    name: action.payload.name,
-                    date: action.payload.date
-                }];
-            }
-            }else if (action.type == "DEL_ITEM") {
-                let name = action.payload.name;
-                newval = CurrentState.filter(tasking => tasking.name != name);
-            }
-              return newval;
-}
+
 function Todo() {
-    // let [tasking, settasking] = useState([]);
-    let [tasking, dispatchtodoitem] = useReducer(reducerfun, []);
-
-    let refname = useRef();
-    let reftask = useRef();
-    let clicked = (event) => {
-    event.preventDefault();
-    let date = reftask.current.value;
-    const name = refname.current.value;
-
-        const newfunitem = {
-            type: "NEW_ITEM",
-            payload: {
-                name,
-                date
-            }
-        }
-        dispatchtodoitem(newfunitem);
-        reftask.current.value = "";
-        refname.current.value = "";
-    }
-    let deleteitem = (name) => {
-        
-        const newfunitem = {
-            type: "DEL_ITEM",
-            payload: {name}
-        }
-        dispatchtodoitem(newfunitem);
-        // settasking(finalitem)
-    }
+  
     return <>
-        <items.Provider value={{
-            tasking: tasking,
-            deleteitem: deleteitem,
-        }}>
+        <TodoItemServiceProvider>
             <div className="container mt-3">
                 <h1 style={{ textAlign: 'center', fontWeight: '800', color: 'white', backgroundColor: 'black', padding: '10px 0px' }} className="mb-3">Task 1</h1>
-                <form className="row" onSubmit={clicked}>
+                <form className="row" onSubmit={() => {clicked}}>
                     <div className="col-sm-12 col-md-6 col-lg-4">
-                        <input type="text" placeholder="Enter current date" ref={refname} className="w-100" />
+                        <input type="text" placeholder="Enter current date" className="w-100" />
                     </div>
                     <div className="col-sm-12 col-md-6 col-lg-3 mt-2 mb-3 mt-lg-0">
-                        <input type="date" placeholder="mm/dd/yyyy" ref={reftask} className="w-100" />
+                        <input type="date" placeholder="mm/dd/yyyy" className="w-100" />
                     </div>
                     <div className="col-sm-12 col-md-6 col-lg-4">
                         <button className="btn btn-success w-auto" >Add Task <IoIosAddCircle style={{ fontSize: '22px', marginTop: '-5px' }} className="ms-1" /> </button>
@@ -68,7 +22,7 @@ function Todo() {
                 </form>
                 <Todo_item />
             </div>
-        </items.Provider>
+        </TodoItemServiceProvider>
     </>
 }
 

252beb094d90c62405ecc12a08a2e58a135b4745
Muaaz Anwar <maazweber@gmail.com>
2024-08-20 11:41:26 -0700

    working sidebar

---
+++ social_app/src/components/post/PostList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Post from './Post'
+
+function PostList() {
+  return (
+    <div className='d-flex justify-content-around'>
+    <Post> </Post>
+    <Post> </Post>
+    <Post> </Post>
+    </div>
+  )
+}
+
+export default PostList
